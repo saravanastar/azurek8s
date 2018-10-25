b764d78a1b953a610140f0c60a642561ac6fad36
Saravanakumar Arunachalam <saravanastar@gmail.com>
2018-10-25 10:11:09 -0500

    Update WelcomeController.java

--- src/main/java/com/azure/spring/demo/controller/WelcomeController.java
+++ src/main/java/com/azure/spring/demo/controller/WelcomeController.java
@@ -1,7 +1,12 @@
 package com.azure.spring.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class WeclomeController {
+    @GetMapping("/")
+    public String get() {
+        return "Application is up and running";
+    }
 }
